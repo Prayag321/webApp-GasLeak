1023eadcf4f7296794eb682e3bc395b8c908fcd4
Prayag <prayagmb20it@student.mes.ac.in>
2024-01-12 13:41:46 +0530

    api calls

--- public/fetchdata.js
+++ public/fetchdata.js
@@ -1,17 +1,32 @@
+let temp = document.getElementById("temperature");
+let humid = document.getElementById("humidity");
+let gas = document.getElementById("gas_concentration");
+let alert = document.getElementById("alert");
+let preousTime = "0";
+
 function getData() {
-    let temp = document.getElementById("temperature")
-    let humid = document.getElementById("humidity")
-    let gas = document.getElementById("gas_concentration")
 
-
-    let data = fetch('https://dummyjson.com/products/2');
+    // https://dummyjson.com/products/2
+    let data = fetch('http://localhost:3000/api/sensordata/');
     data.then((val) => {
         return val.json();
     }).then((val) => {
-            temp.textContent = val.title;
-            console.log(val.title);
+        if (preousTime !== (val.dbData[0].createdAt.split('T')[1]) ) {
+            temp.textContent = val.dbData[0].temperature;
+            humid.textContent = val.dbData[0].temperature;
+            gas.textContent = val.dbData[0].gasreading;
+            preousTime = val.dbData[0].createdAt.split('T')[1];
+            alert.textContent = "Space for alert";
+        }
+        else{
+            alert.textContent = "Device is dissconnected";
+            alert.style.color = "red"
+            alert.style.fontSize = "20px"
+        }
     })
 
+
+
 }
-// setInterval(getData,1000); //for execution 
+setInterval(getData, 1100); //for execution 
 
