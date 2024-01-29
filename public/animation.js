6b17bf6cccf3c2a9868c3acbd017568c6b1571ad
Prayag <prayagmb20it@student.mes.ac.in>
2024-01-29 14:04:48 +0530

    bug fix

--- public/animation.js
+++ public/animation.js
@@ -2,7 +2,7 @@
 gsap.from(".goupbtn", {
     opacity: 0,
     duration: .9,
-    y:50,
+    y: 50,
     scrollTrigger: {
         trigger: "#manual",
         scroller: "body",
@@ -10,3 +10,54 @@ gsap.from(".goupbtn", {
         toggleActions: "play none none reverse",
     }
 })
+
+// Navbar Animation
+gsap.from("nav", {
+    opacity: 0,
+    duration: .8,
+    y: -50,
+    delay: 0.4,
+    ease: "power2.inOut"
+});
+
+// Carousel Animation
+gsap.from(".carousel", {
+    opacity: 0,
+    duration: 2,
+    delay: 0.4,
+    ease: "power2.inOut"
+});
+
+// Card Animation
+gsap.from(".card-container", {
+    opacity: 0,
+    y: 100,
+    stagger: 0.2,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".card-container",
+        start: "top 80%",
+        end: "bottom 60%",
+        scrub: 1,
+        toggleActions: "play none none reverse"
+
+    }
+});
+
+// real time sensor info
+gsap.from(".card-client", {
+    opacity: 0,
+    y: 100,
+    stagger: 0.2,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".cantactus",
+        start: "top 40%",
+        end: "bottom 80%",
+        scrub: 1,
+        toggleActions: "play none none reverse",
+        markers:"true"
+
+    }
+});
+
