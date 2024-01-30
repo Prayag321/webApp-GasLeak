a08a3e7470e27bf14fe9614093f4044e58ae4daa
Prayag <prayagmb20it@student.mes.ac.in>
2024-01-30 20:39:22 +0530

    Fix Fetch

--- public/fetchdata.js
+++ public/fetchdata.js
@@ -2,34 +2,41 @@ let temp = document.getElementById("temperature");
 let humid = document.getElementById("humidity");
 let gas = document.getElementById("gas_concentration");
 let alert = document.getElementById("alert");
-let preousTime = "0";
+let preousTime = "";
 let knobstatus = document.getElementById("motor_s");
 
 const currentIP = window.location.hostname;
 // Define the base URL for the fetch request
-const baseUrl = (currentIP === 'localhost' || currentIP === '127.0.0.1') ? 'http://localhost:3000' : 'https://your-other-api-url';
+const baseUrl = (currentIP === 'localhost' || currentIP === '127.0.0.1') ? 'http://localhost:3000' : 'https://nodeserver-gasleak-production.up.railway.app';
 // Append the endpoint to the base URL
 const apiUrl = `${baseUrl}/api/sensordata/`;
 
 function getData() {
     // https://dummyjson.com/products/2    test api
     // 7veY3rCXY872zflm  atlas pass
-    // preousTime !== (val.dbData[0].createdAt.split('T')[1])
+    // preousTime !== (val.dbData[0].createdAt.split('T')[1])  not working cause seconds nay he
     let data = fetch(apiUrl);
     data.then((val) => {
         return val.json();
     }).then((val) => {
         if (true) { 
+            // console.log(`${val.dbData[0].createdAt.split('T')[0]}`);
             temp.textContent = val.dbData[0].temperature;
             humid.textContent = val.dbData[0].humidity;
             gas.textContent = val.dbData[0].gasreading;
-            knobstatus.textContent = val.dbData[0].nobstatus;
-            preousTime = val.dbData[0].createdAt.split('T')[1];
+            // preousTime = val.dbData[0].createdAt.split('T')[0];
+            preousTime = val.dbData[0].createdAt;
+            if (val.dbData[0].nobstatus == 1) {
+                knobstatus.textContent = "OFF";  
+            }
+            else{
+                knobstatus.textContent = "ON";
+            }
             alert.textContent = "Space for alert";
 
         }
         else {
-            alert.textContent = "Device is dissconnected";
+            alert.textContent = "Device is dissconnected"; 
             alert.style.color = "red"
             alert.style.fontSize = "20px"
             alert.style.display = "flex"
@@ -38,7 +45,7 @@ function getData() {
     })
     .catch(error => {
     console.error('Error fetching data:', error)
-    alert.textContent = "Error fetching data";
+    alert.textContent = `Server is not connected`;
     alert.style.color = "red"
     alert.style.fontSize = "20px"
     alert.style.display = "flex"
