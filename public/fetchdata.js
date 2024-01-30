711f6cb381d9a7e4ec41bbc1a59a5f6ecb9bfbc7
Prayag <prayagmb20it@student.mes.ac.in>
2024-01-30 08:44:06 +0530

    Bug Fix Api call

--- public/fetchdata.js
+++ public/fetchdata.js
@@ -5,19 +5,25 @@ let alert = document.getElementById("alert");
 let preousTime = "0";
 let knobstatus = document.getElementById("motor_s");
 
-function getData() {
+const currentIP = window.location.hostname;
+// Define the base URL for the fetch request
+const baseUrl = (currentIP === 'localhost' || currentIP === '127.0.0.1') ? 'http://localhost:3000' : 'https://your-other-api-url';
+// Append the endpoint to the base URL
+const apiUrl = `${baseUrl}/api/sensordata/`;
 
+function getData() {
     // https://dummyjson.com/products/2    test api
     // 7veY3rCXY872zflm  atlas pass
-    let data = fetch('http://localhost:3000/api/sensordata/');
+    // preousTime !== (val.dbData[0].createdAt.split('T')[1])
+    let data = fetch(apiUrl);
     data.then((val) => {
         return val.json();
     }).then((val) => {
-        if (preousTime !== (val.dbData[0].createdAt.split('T')[1])) {
+        if (true) { 
             temp.textContent = val.dbData[0].temperature;
             humid.textContent = val.dbData[0].humidity;
             gas.textContent = val.dbData[0].gasreading;
-            knobstatus.textContent = val.dbData[0].knobstatus;
+            knobstatus.textContent = val.dbData[0].nobstatus;
             preousTime = val.dbData[0].createdAt.split('T')[1];
             alert.textContent = "Space for alert";
 
@@ -32,7 +38,7 @@ function getData() {
     })
     .catch(error => {
     console.error('Error fetching data:', error)
-    alert.textContent = "Device is dissconnected";
+    alert.textContent = "Error fetching data";
     alert.style.color = "red"
     alert.style.fontSize = "20px"
     alert.style.display = "flex"
